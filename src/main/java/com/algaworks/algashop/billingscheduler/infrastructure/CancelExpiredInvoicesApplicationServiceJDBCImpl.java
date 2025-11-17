acc455b1189361cb0e0b24515741a3b5c07c083b
RE045908 <re045908@qintess.com>
2025-11-17 15:05:07 -0300

    SMS level 3 - 9.6 - Processing data with JdbcTemplate

--- src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoicesApplicationServiceJDBCImpl.java
+++ src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoicesApplicationServiceJDBCImpl.java
@@ -1,14 +1,72 @@
 package com.algaworks.algashop.billingscheduler.infrastructure;
 
 import com.algaworks.algashop.billingscheduler.application.CancelExpiredInvoicesApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class CancelExpiredInvoicesApplicationServiceJDBCImpl implements CancelExpiredInvoicesApplicationService {
 
+    private final JdbcOperations jdbcOperations;
+
+    private static final Duration EXPIRED_SINCE = Duration.ofDays(1);
+
+    private static final String UNPAID_STATUS = "UNPAID";
+
+    private static final String CANCELED_STATUS = "CANCELED";
+
+    private static final String CANCELED_REASON = "Invoice Expired";
+
+    private static final String SELECT_EXPIRED_INVOICES_SQL = String.format("""
+            select id
+            from invoice i
+            where i.expires_at <= now() - interval '%d days'
+                and i.status = ?
+    """, EXPIRED_SINCE.toDays());
+
+    private static final String UPDATE_INVOICE_STATUS_SQL = """
+            update invoice i set status = ? , canceled_at = now(), cancel_reason = ?
+            where id = ?
+    """;
 
     @Override
     public void cancelExpiredInvoices() {
+        List<UUID> invoiceIds = fetchExpiredInvoices();
+        log.info("Tasks - Total invoices fetched: {}", invoiceIds.size());
+        int totalCanceledInvoices = cancelInvoices(invoiceIds);
+        log.info("Tasks - Total invoices canceled: {}", totalCanceledInvoices);
+    }
 
+    private int cancelInvoices(List<UUID> invoiceIds){
+        int updatedInvoices = 0;
+        for (UUID invoiceId: invoiceIds){
+            try {
+                jdbcOperations.update(UPDATE_INVOICE_STATUS_SQL, CANCELED_STATUS, CANCELED_REASON, invoiceId);
+                updatedInvoices++;
+                log.info("Task - Invoice canceled ID {}", invoiceId);
+            } catch (DataAccessException e){
+                log.error("Task - Failed to canceled invoice with ID {}", invoiceId, e);
+            }
+        }
+        return updatedInvoices;
+    }
+
+    private List<UUID> fetchExpiredInvoices(){
+        PreparedStatementSetter preparedStatementSetter = ps -> {
+            ps.setString(1, UNPAID_STATUS);
+        };
+        RowMapper<UUID> mapper = (resultSet, rowNum) -> resultSet.getObject("id", UUID.class);
+        return jdbcOperations.query(SELECT_EXPIRED_INVOICES_SQL, preparedStatementSetter, mapper);
     }
 }
