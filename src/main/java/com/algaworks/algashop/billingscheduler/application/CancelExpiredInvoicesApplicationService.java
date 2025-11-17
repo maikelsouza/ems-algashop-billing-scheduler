c361f5c8dda668b0c5d9dac7d39a90b3c0372341
RE045908 <re045908@qintess.com>
2025-11-17 13:41:09 -0300

    SMS level 3 - 9.5 - Implementing Scheduling with Spring

---
+++ src/main/java/com/algaworks/algashop/billingscheduler/application/CancelExpiredInvoicesApplicationService.java
@@ -0,0 +1,6 @@
+package com.algaworks.algashop.billingscheduler.application;
+
+public interface CancelExpiredInvoicesApplicationService {
+
+    void cancelExpiredInvoices();
+}
---
+++ src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoicesScheduler.java
@@ -0,0 +1,24 @@
+package com.algaworks.algashop.billingscheduler.infrastructure;
+
+import com.algaworks.algashop.billingscheduler.application.CancelExpiredInvoicesApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class CancelExpiredInvoicesScheduler {
+
+    private final CancelExpiredInvoicesApplicationService applicationService;
+
+    @Scheduled(fixedRate = 5000) // every 5 seconds
+    public void runTask(){
+        log.info("Task started - Cancelling expired invoices.");
+        applicationService.cancelExpiredInvoices();
+        log.info("Task ended - Expired invoices.");
+
+
+    }
+}
